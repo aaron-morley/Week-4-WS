c71f253b83b048fe4d2d42c6effa75290593da7e
aaron-morley <111508607+aaron-morley@users.noreply.github.com>
2022-08-22 23:48:53 +0930

    Add files via upload

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 using namespace std;
 
+int *t = new int;
+
 int size_of_variable_star_t() {
         return sizeof(*t);
     }
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -4,8 +4,6 @@
 using namespace std;
 #include "function-1-1.cpp"
 
-int *t = new int;
-
 int main()
 {
     cout << size_of_variable_star_t() << endl;
